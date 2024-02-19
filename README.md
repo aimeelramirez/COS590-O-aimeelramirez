216953f462f057143da5684b2219b7f4fc277fd2
aimee <alramirez1@alumni.fullsail.edu>
2024-02-19 04:57:30 -0500

    example of plaid with react-native temp

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # COS590-O-aimeelramirez
+![alt text](one.png)
--- client/src/screens/PlaidScreen.tsx
+++ client/src/screens/PlaidScreen.tsx
@@ -1,7 +1,7 @@
-// PlaidScreen.tsx
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import PlaidLinkScreen from './PlaidLinkScreen';
+import { LinkExit, LinkSuccess } from 'react-native-plaid-link-sdk';
 
 const PlaidScreen: React.FC = () => {
   const [token, setToken] = React.useState('');
@@ -25,12 +25,44 @@ const PlaidScreen: React.FC = () => {
 
   React.useEffect(() => {
     test();
-    console.log(token)
+    console.log(token);
   }, []);
+
+  const handleSuccess = useCallback(
+    async (success: LinkSuccess) => {
+      console.log(success);
+
+      try {
+        const response = await fetch(`http://localhost:8000/api/exchange_public_token`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ public_token: success.publicToken }),
+        });
+
+        if (!response.ok) {
+          console.log('Error exchanging public token:', response.statusText);
+        } else {
+          console.log('Public token exchanged successfully');
+        }
+      } catch (err) {
+        console.error('Error exchanging public token:', err);
+      }
+    },
+    [] // Dependency array is empty as there are no dependencies
+  );
 
   return (
     <View style={styles.container}>
-      <PlaidLinkScreen token={token} />
+      <PlaidLinkScreen
+        token={token}
+        publicToken
+        onSuccess={handleSuccess}
+        onExit={(response: LinkExit) => {
+          console.log(response);
+        }}
+      />
       <Text style={{ color: 'black' }}>Test Plaid Screen</Text>
     </View>
   );
